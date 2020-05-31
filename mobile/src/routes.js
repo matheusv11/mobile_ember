a7845cd273231c9153a8c27683c18cb410b960f9
matheusv11 <matheusv11@MacBook-Pro-de-matheusv11.local>
2020-05-31 02:35:43 -0300

    ok

---
+++ mobile/src/routes.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+
+//import { Text, View } from 'react-native';
+
+const Tab = createMaterialTopTabNavigator();
+
+const Routes= ()=>{
+    return(
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}}>
+                <NavigationContainer>
+
+                    <Tab.Navigator tabBarPosition='bottom'>
+                        <Tab.Screen name="Home" component={Home} />
+                        <Tab.Screen name="Profile" component={Profile} />
+                    </Tab.Navigator>
+
+                </NavigationContainer>
+            </SafeAreaView>
+      </SafeAreaProvider>
+    )
+}
+export default Routes;
+
+
