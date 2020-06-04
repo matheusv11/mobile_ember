86f796006e62eb6f6a7e603957964369c4f42eac
matheusv11 <matheussales360@hotmail.com>
2020-06-04 04:18:20 -0300

    ok

--- mobile/src/pages/Register/index.js
+++ mobile/src/pages/Register/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, Image} from 'react-native'
+import { View, Text, TouchableOpacity, TextInput, Image, KeyboardAvoidingView} from 'react-native'
 import { useNavigation } from '@react-navigation/native';
 import * as ImagePicker from 'expo-image-picker';//Retorna todos metodos, funcoes pro ImagePicker
 
@@ -10,7 +10,13 @@ const Register= ()=>{
 
 //Constantes
     const navigation= useNavigation();
+    
     const [image,setImage]= useState(null);
+    const [dados,setDados]= useState({
+        name: '',
+        email: '',
+        password: ''
+    })
 
 //Funcoes
     const goLogin= ()=>{
@@ -38,17 +44,21 @@ const Register= ()=>{
         //Tentar otimizar e trazer nome original
         //Fazer edicao de imagem pra encaixa-la no avatar
         //Verificar novamente os dados do formData enviados pro back no lemon
+        //const data= {name,email,password}
         const localUri = image.uri;
         const filename = localUri.split('/').pop();
-        
         const match = /\.(\w+)$/.exec(filename);
         const type = match ? `image/${match[1]}` : `image`;
       
         const formData = new FormData();
-        formData.append('file', { uri: localUri, name: filename, type });
+        formData.append('file', { uri: localUri, name: filename, type });//Ver se pode passar array com outros campos do body
+        formData.append('name', dados.name)
+        formData.append('email', dados.email)
+        formData.append('password', dados.password)
         console.log(formData)
-
+  
         //IF IMAGEM NULL
+
         try{
             await api.post('/image', formData,{
                 headers:{'Content-Type': 'multipart/form-data'}
@@ -57,14 +67,17 @@ const Register= ()=>{
         catch{
             alert('erro')
         }
+
+       
     }
     
 //Retorno
     return(
 
         <View style={styles.container} >
+            <KeyboardAvoidingView behavior="position" >
             <View style={styles.body}>
-    
+                
                 <Text style={styles.containerText}>Ember</Text>
                 
                 <TouchableOpacity onPress={Preview} style={styles.preview}>
@@ -73,10 +86,10 @@ const Register= ()=>{
                     <Text>Select the image</Text>
                 }
                 </TouchableOpacity>
-
-                <TextInput style={styles.input} placeholder="Nome"/>
-                <TextInput style={styles.input} placeholder="Email"/>
-                <TextInput style={styles.input} placeholder="Senha"/>
+             
+                <TextInput style={styles.input} placeholder="Nome" onChangeText={name=> setDados({...dados, name: name})}/>
+                <TextInput style={styles.input} placeholder="Email"  onChangeText={email=> setDados({...dados, email: email})}/>
+                <TextInput style={styles.input} placeholder="Senha"  onChangeText={password=> setDados({...dados, password})}/>
 
                 
                 <TouchableOpacity style={styles.button} onPress={Register}>
@@ -84,7 +97,8 @@ const Register= ()=>{
                 </TouchableOpacity>
             </View>
 
-            
+            </KeyboardAvoidingView>
+
             <View style={styles.bottom}>
                 <Text>Possui cadastro? </Text>
 
